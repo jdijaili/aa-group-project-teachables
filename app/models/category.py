395d6d53f9350278bd96088ce948c6dd32687a7f
nathaniel <nathanieltseng@gmail.com>
2022-02-04 16:19:11 -0800

    DB: create models Fixes #42

---
+++ app/models/category.py
@@ -0,0 +1,20 @@
+from .db import db
+
+
+class Category(db.Model):
+    __tablename__ = "categories"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    image = db.Column(db.String, nullable=False)
+
+    project = db.relationship("Category", back_populates="category")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "image": self.image
+        }
