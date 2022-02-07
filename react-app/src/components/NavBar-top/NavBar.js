278936a552c3421ce33aea7ed37b294ce2f17aa7
Yu Ra Kim <yurakim.designs@gmail.com>
2022-02-07 14:06:57 -0500

    Rendering and css for two-part navbar, conditional rendering based on user login status

---
+++ react-app/src/components/NavBar-top/NavBar.js
@@ -0,0 +1,53 @@
+
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import LogoutButton from '../auth/LogoutButton';
+import LoginSignup from '../auth/LoginSignup';
+import './NavBar.css';
+import NavBarBottom from '../NavBar-bottom/NavBarBottom';
+
+const NavBar = () => {
+  const user = useSelector(state => state.session.user)
+  return (
+    <div>
+      <nav>
+        <ul className='site-options'>
+          <li>
+            <NavLink to='/' exact={true} activeClassName='active'>
+              <i class="fas fa-home" />
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/' exact={true}>
+              Chess Openings
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/' exact={true}>
+              Game Development
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/' exact={true}>
+              Jewellery Design
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/' exact={true}>
+              Knitting
+            </NavLink>
+          </li>
+        </ul>
+        <ul className='user-options'>
+          {(user)? <LogoutButton /> : <LoginSignup />}
+
+
+        </ul>
+      </nav>
+      <NavBarBottom />
+    </div>
+  );
+}
+
+export default NavBar;
