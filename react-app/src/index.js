02d0a20d96121c855c1c54312ea574aeaf2a2957
nathaniel <nathanieltseng@gmail.com>
2022-02-07 10:47:33 -0800

    Create redux store Fixes #43

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,14 +4,22 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import configureStore from './store';
+import * as sessionActions from "./store/session";
+import * as projectActions from "./store/projects";
 
 const store = configureStore();
 
+if (process.env.NODE_ENV !== "production") {
+	window.store = store;
+	window.sessionActions = sessionActions;
+	window.projectActions = projectActions;
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-        <App />
-      </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
