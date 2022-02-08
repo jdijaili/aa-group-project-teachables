ac59ef399477fc8016fab5cc712c586661935ca6
jdijaili <jennifer.dijailirx@gmail.com>
2022-02-08 12:47:56 -0800

    created flask forms for project and step

--- app/forms/project_form.py
+++ app/forms/project_form.py
@@ -1,5 +1,11 @@
+from random import choices
 from flask_wtf import FlaskForm
-from wtforms import DateField, StringField, IntegerField, SelectField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, TextAreaField, SelectField, SubmitField
+from wtforms.validators import DataRequired, Length
 
-
+class PublishForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired(message='This field must not be left blank.'), Length(max=50, message='Title must not be greater than 50 character.')])
+    description = TextAreaField("Description", validators=[DataRequired(message='This field must not be left blank.')])
+    category_id = SelectField("Category", choices=[(1, 'Chess Openings'), (2, 'Game Development'), (3, 'Jewelry Design'), (4, 'Knitting')], coerce=int, validators=[DataRequired(message='This field must not be left blank.')])
+    supplies_text = TextAreaField("Supplies")
+    supplies_image = StringField("Supplies Image")
