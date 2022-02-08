bef9c80ed849dbab62c4d86d838267772ab92c00
Yu Ra Kim <yurakim.designs@gmail.com>
2022-02-08 16:07:41 -0500

    initial styling for categories cards, links in navbar now redirect to categories, each project card redirects to project page

--- react-app/src/components/CategoriesView/CategoriesView.js
+++ react-app/src/components/CategoriesView/CategoriesView.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { getProjects } from '../../store/projects';
 import { getCategories } from '../../store/categories';
 import './CategoriesView.css';
@@ -25,23 +25,38 @@ const CategoriesView = () => {
         return state.categories
     });
     const selectedCategory = Object.values(allCategories).filter(category => category.id == categoryId);
+    const categoryArr = Object.values(selectedCategory);
 
     return (
-        <div className='categories-body'>
-            <h1>Now Viewing the {selectedCategory[0].name} Category</h1>
-            <div className='categories-cards'>
-                {projectsArr.map(project => {
-                    return (
-                        <div className='categories-card'>
-                            <h2>{project.title}</h2>
-                            {/* TO DO: Replace with user info from user store */}
-                            <p>By user {project.user_id}</p>
-                            <p>{project.description}</p>
-                        </div>
-                    )
-                })}
+        <div className='categories-page'>
+            {categoryArr.map(category => {
+                return (
+                    <div className='category-header'>
+                        <div className={categoryArr[0].id == 1 ? 'categories-chess' :
+                            categoryArr[0].id == 3 ? 'categories-gamedev' :
+                                categoryArr[0].id == 4 ? 'categories-jewelry' :
+                                    categoryArr[0].id == 2 ? 'categories-knitting' : ''} />
+                        <h1 className='category-header-text'>Now Viewing the {categoryArr[0].name} Category</h1>
+                    </div>
+                )
+            })};
+            <div className='categories-body'>
+                <div className='categories-cards'>
+                    {projectsArr.map(project => {
+                        return (
+                            <div className='categories-card' Link to={`/projects/${project.id}`}>
+                                <Link to={`/projects/${project.id}`}><img className='card-image' src={project.img} /></Link>
+                                <div className='categories-text'>
+                                    <Link to={`/projects/${project.id}`}><h3>{project.title}</h3></Link>
+                                    {/* TO DO: Replace with user info from user store */}
+                                    <p className='categories-p'>By user {project.user_id}</p>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
-        </div>
+        </div >
     )
 };
 
