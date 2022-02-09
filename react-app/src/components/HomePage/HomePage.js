44899975870c134747a3bb01e543442022788392
Yu Ra Kim <yurakim.designs@gmail.com>
2022-02-09 11:26:17 -0500

    Added dispatch to dependency array to remove console warning

--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -12,7 +12,7 @@ const Home = () => {
 
     useEffect(() => {
         dispatch(getProjects());
-    }, []);
+    }, [dispatch]);
 
     const allProjects = useSelector(state => {
         return state.projects
