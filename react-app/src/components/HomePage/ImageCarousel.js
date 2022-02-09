24c47a291d7353cee3defb53c42a2b9d6259cb14
nathaniel <nathanieltseng@gmail.com>
2022-02-09 09:06:50 -0800

    resolved alt text warnings

--- react-app/src/components/HomePage/ImageCarousel.js
+++ react-app/src/components/HomePage/ImageCarousel.js
@@ -3,28 +3,32 @@ import { Carousel } from 'react-responsive-carousel';
 import './ImageCarousel.css'
 
 const ImageCarousel = () => {
-    return(
-        <div className='carousel'>
-            <Carousel showThumbs={false} showArrows={false} showStatus={false} infiniteLoop={true} autoPlay={true} interval={4000}>
-                <div>
-                    <img src="https://images.unsplash.com/photo-1642056876324-39a910bc9dba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" />
-                </div>
-                <div>
-                    <img src="https://images.unsplash.com/photo-1593720219276-0b1eacd0aef4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1761&q=80" />
-                </div>
-                <div>
-                    <img src="https://images.unsplash.com/photo-1629481995102-ff98d306dd8a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1734&q=80" />
-                </div>
-                <div>
-                    <img src="https://images.unsplash.com/photo-1628759213613-40de37caaf32?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1774&q=80" />
-                </div>
-            </Carousel>
-            <div className='overlay'>
-                <h1 className='overlay-title'>YOURS FOR THE MAKING</h1>
-                <h3 className='overlay-text'>Teachables is a community for people who like to make things. Come explore, share, and make your next project with us!</h3>
-            </div>
-        </div>
-    )
+	return (
+		<div className='carousel'>
+			<Carousel showThumbs={false} showArrows={false} showStatus={false} infiniteLoop={true} autoPlay={true} interval={4000}>
+				<div>
+					<img src="https://images.unsplash.com/photo-1642056876324-39a910bc9dba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+						alt="Chess Openings" />
+				</div>
+				<div>
+					<img src="https://images.unsplash.com/photo-1593720219276-0b1eacd0aef4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1761&q=80"
+						alt="Game Development" />
+				</div>
+				<div>
+					<img src="https://images.unsplash.com/photo-1629481995102-ff98d306dd8a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1734&q=80"
+						alt="Jewlery Design" />
+				</div>
+				<div>
+					<img src="https://images.unsplash.com/photo-1628759213613-40de37caaf32?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1774&q=80"
+						alt="Knitting" />
+				</div>
+			</Carousel>
+			<div className='overlay'>
+				<h1 className='overlay-title'>YOURS FOR THE MAKING</h1>
+				<h3 className='overlay-text'>Teachables is a community for people who like to make things. Come explore, share, and make your next project with us!</h3>
+			</div>
+		</div>
+	)
 }
 
 export default ImageCarousel
--- react-app/src/components/ProjectCard/ProjectCard.js
+++ react-app/src/components/ProjectCard/ProjectCard.js
@@ -4,7 +4,7 @@ import './ProjectCard.css';
 const ProjectCard = ({ project }) => {
 	return (
 		<Link to={`/projects/${project.id}`}><div className='explorer-card' key={project.id}>
-			<Link to={`/projects/${project.id}`}><img className='explorer-image' src={project.suppliesImage ? project.suppliesImage : '/images/noimage.png'} /></Link>
+			<Link to={`/projects/${project.id}`}><img className='explorer-image' src={project.suppliesImage ? project.suppliesImage : '/images/noimage.png'} alt={project.title}/></Link>
 			<div className='explorer-text'>
 				<Link to={`/projects/${project.id}`}><h3 className='explorer-links'>{project.title}</h3></Link>
 				{/* TODO: #84 Replace with user info from user store */}
