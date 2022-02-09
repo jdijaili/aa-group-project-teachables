b5ccedb06a1bc7e65e6c892fdfa2d1cdb86848dd
Yu Ra Kim <yurakim.designs@gmail.com>
2022-02-09 11:23:58 -0500

    Links replaced with a tags to remove Link console error

--- react-app/src/components/ProjectCard/ProjectCard.js
+++ react-app/src/components/ProjectCard/ProjectCard.js
@@ -1,14 +1,13 @@
-import { Link } from 'react-router-dom';
 import './ProjectCard.css';
 
 const ProjectCard = ({ project }) => {
 	return (
-		<div className='explorer-card' Link to={`/projects/${project.id} key=${project.id}`}>
-			<Link to={`/projects/${project.id}`}><img className='explorer-image' src={project.suppliesImage ? project.suppliesImage : '/images/noimage.png'} /></Link>
+		<div className='explorer-card' a href={`/projects/${project.id} key=${project.id}`}>
+			<a href={`/projects/${project.id}`}><img className='explorer-image' src={project.suppliesImage ? project.suppliesImage : '/images/noimage.png'} /></a>
 			<div className='explorer-text'>
-				<Link to={`/projects/${project.id}`}><h3 className='explorer-links'>{project.title}</h3></Link>
+				<a href={`/projects/${project.id}`}><h3 className='explorer-links'>{project.title}</h3></a>
 				{/* TODO: #84 Replace with user info from user store */}
-				<p className='explorer-user'>By user {project.userId}</p>
+				<a href={`/users/${project.userId}`}><p className='explorer-user'>By user {project.userId}</p></a>
 			</div>
 		</div>
 	)
