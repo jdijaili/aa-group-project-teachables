d92d6c3ac4a4c5472779cff03f19830faf7be484
Nathaniel Tseng <nathanieltseng@gmail.com>
2022-02-09 10:15:38 -0800

    demo user functionality

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -1,69 +1,68 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link, Redirect } from 'react-router-dom';
-import { login } from '../../store/session';
+import { login, demoLogin } from '../../store/session';
 import './Auth.css';
 
 const LoginForm = () => {
-  const [errors, setErrors] = useState([]);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const user = useSelector(state => state.session.user);
-  const dispatch = useDispatch();
+	const [errors, setErrors] = useState([]);
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const user = useSelector(state => state.session.user);
+	const dispatch = useDispatch();
 
-  const onLogin = async (e) => {
-    e.preventDefault();
-    const data = await dispatch(login(email, password));
-    if (data) {
-      setErrors(data);
-    }
-  };
+	const onLogin = async (e) => {
+		e.preventDefault();
+		const data = await dispatch(login(email, password));
+		if (data) {
+			setErrors(data);
+		}
+	};
 
-  const updateEmail = (e) => {
-    setEmail(e.target.value);
-  };
+	const updateEmail = (e) => {
+		setEmail(e.target.value);
+	};
 
-  const updatePassword = (e) => {
-    setPassword(e.target.value);
-  };
+	const updatePassword = (e) => {
+		setPassword(e.target.value);
+	};
 
-  if (user) {
-    return <Redirect to='/' />;
-  }
+	if (user) {
+		return <Redirect to='/' />;
+	}
 
-  return (
-    <div className='login-body'>
-      <form className='auth-form' onSubmit={onLogin}>
-        <input className='auth-input'
-          name='email'
-          type='text'
-          placeholder='Email'
-          value={email}
-          required={true}
-          onChange={updateEmail}
-        />
-        <input className='auth-input'
-          name='password'
-          type='password'
-          placeholder='Password'
-          value={password}
-          required={true}
-          onChange={updatePassword}
-        />
-        <button className='auth-button' type='submit'>Login</button>
-        <div className='auth-errors'>
-          {errors.map((error, ind) => (
-            <div key={ind}>{error}</div>
-          ))}
-        </div>
-        <div className='auth-options'>
-          <p>New to Teachables? <Link className='auth-links' to='/sign-up'>Sign up >></Link></p>
-          {/* TODO: Add demo user login functionality */}
-          <p><Link className='auth-links'>Continue as demo user >></Link></p>
-        </div>
-      </form>
-    </div>
-  );
+	return (
+		<div className='login-body'>
+			<form className='auth-form' onSubmit={onLogin}>
+				<input className='auth-input'
+					name='email'
+					type='text'
+					placeholder='Email'
+					value={email}
+					required={true}
+					onChange={updateEmail}
+				/>
+				<input className='auth-input'
+					name='password'
+					type='password'
+					placeholder='Password'
+					value={password}
+					required={true}
+					onChange={updatePassword}
+				/>
+				<button className='auth-button' type='submit'>Login</button>
+				<div className='auth-errors'>
+					{errors.map((error, ind) => (
+						<div key={ind}>{error}</div>
+					))}
+				</div>
+				<div className='auth-options'>
+					<p>New to Teachables? <Link className='auth-links' to='/sign-up'>Sign up {'>>'}</Link></p>
+					<p><span className='auth-links' onClick={e => dispatch(demoLogin())}>Continue as demo user {'>>'}</span></p>
+				</div>
+			</form>
+		</div>
+	);
 };
 
 export default LoginForm;
--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -1,91 +1,92 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { Link, Redirect } from 'react-router-dom';
-import { signUp } from '../../store/session';
+import { signUp, demoLogin } from '../../store/session';
 
 const SignUpForm = () => {
-  const [errors, setErrors] = useState([]);
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [repeatPassword, setRepeatPassword] = useState('');
-  const user = useSelector(state => state.session.user);
-  const dispatch = useDispatch();
+	const [errors, setErrors] = useState([]);
+	const [username, setUsername] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [repeatPassword, setRepeatPassword] = useState('');
+	const user = useSelector(state => state.session.user);
+	const dispatch = useDispatch();
 
-  const onSignUp = async (e) => {
-    e.preventDefault();
-    if (password === repeatPassword) {
-      const data = await dispatch(signUp(username, email, password));
-      if (data) {
-        setErrors(data)
-      }
-    }
-  };
+	const onSignUp = async (e) => {
+		e.preventDefault();
+		if (password === repeatPassword) {
+			const data = await dispatch(signUp(username, email, password));
+			if (data) {
+				setErrors(data)
+			}
+		}
+	};
 
-  const updateUsername = (e) => {
-    setUsername(e.target.value);
-  };
+	const updateUsername = (e) => {
+		setUsername(e.target.value);
+	};
 
-  const updateEmail = (e) => {
-    setEmail(e.target.value);
-  };
+	const updateEmail = (e) => {
+		setEmail(e.target.value);
+	};
 
-  const updatePassword = (e) => {
-    setPassword(e.target.value);
-  };
+	const updatePassword = (e) => {
+		setPassword(e.target.value);
+	};
 
-  const updateRepeatPassword = (e) => {
-    setRepeatPassword(e.target.value);
-  };
+	const updateRepeatPassword = (e) => {
+		setRepeatPassword(e.target.value);
+	};
 
-  if (user) {
-    return <Redirect to='/' />;
-  }
+	if (user) {
+		return <Redirect to='/' />;
+	}
 
-  return (
-    <div className='signup-body'>
-      <form className='auth-form' onSubmit={onSignUp}>
-        <input className='auth-input'
-          type='text'
-          name='username'
-          onChange={updateUsername}
-          value={username}
-          placeholder='Username'
-        ></input>
-        <input className='auth-input'
-          type='text'
-          name='email'
-          onChange={updateEmail}
-          value={email}
-          placeholder='Email'
-        ></input>
-        <input className='auth-input'
-          type='password'
-          name='password'
-          onChange={updatePassword}
-          value={password}
-          placeholder='Password'
-        ></input>
-        <input className='auth-input'
-          type='password'
-          name='repeat_password'
-          onChange={updateRepeatPassword}
-          value={repeatPassword}
-          required={true}
-          placeholder='Enter password again'
-        ></input>
-        <button className='auth-button' type='submit'>Sign Me Up!</button>
-        <div className='auth-errors'>
-          {errors.map((error, ind) => (
-            <div key={ind}>{error}</div>
-          ))}
-        </div>
-        <div className='auth-options'>
-          <p>Already have an account? <Link className='auth-links' to='/login'>Log in >></Link></p>
-        </div>
-      </form >
-    </div >
-  );
+	return (
+		<div className='signup-body'>
+			<form className='auth-form' onSubmit={onSignUp}>
+				<input className='auth-input'
+					type='text'
+					name='username'
+					onChange={updateUsername}
+					value={username}
+					placeholder='Username'
+				></input>
+				<input className='auth-input'
+					type='text'
+					name='email'
+					onChange={updateEmail}
+					value={email}
+					placeholder='Email'
+				></input>
+				<input className='auth-input'
+					type='password'
+					name='password'
+					onChange={updatePassword}
+					value={password}
+					placeholder='Password'
+				></input>
+				<input className='auth-input'
+					type='password'
+					name='repeat_password'
+					onChange={updateRepeatPassword}
+					value={repeatPassword}
+					required={true}
+					placeholder='Enter password again'
+				></input>
+				<button className='auth-button' type='submit'>Sign Me Up!</button>
+				<div className='auth-errors'>
+					{errors.map((error, ind) => (
+						<div key={ind}>{error}</div>
+					))}
+				</div>
+				<div className='auth-options'>
+					<p>Already have an account? <Link className='auth-links' to='/login'>Log in {'>>'}</Link></p>
+					<p><span className='auth-links' onClick={e => dispatch(demoLogin())}>Continue as demo user {'>>'}</span></p>
+				</div>
+			</form >
+		</div >
+	);
 };
 
 export default SignUpForm;
