88e3e812dfd6d7339c67f2ba0357bbfe09bcff1e
jdijaili <jennifer.dijailirx@gmail.com>
2022-02-10 08:45:13 -0800

    project_image for jewelry projects

--- app/seeds/projects.py
+++ app/seeds/projects.py
@@ -16,6 +16,7 @@ def seed_projects():
         user_id=10,
         title="Beaded Cherry Bracelet",
         description="There's nothing better than spotting the first signs of summer at the market. Cherries and peaches are a household favorite! In celebration of the arrival of a new season, I'll be showing you how to add beaded cherries to your summer stack. It's extremely easy and can be incorporated in a variety of ways! I love mixing the deep red and green seed beads with delicate freshwater pearls - but you can also add other seed beads to the mix.",
+        project_image="/images/jewelry/cherry-bracelet/diycherryproject1.jpeg",
         category_id=4,
         supplies_text="- Cherry Red & Pine Green 8/0 frosted seed beads - Milky White 8/0 seed beads or 2.5mm freshwater pearls - 0.5mm elastic beading cord - Scissors - Scotch tape - Clear nail polish",
         supplies_image="/images/jewelry/cherry-bracelet/diycherry1.jpeg"
@@ -25,6 +26,7 @@ def seed_projects():
         user_id=10,
         title="Tila Bead Bracelet",
         description="The Tila bead bracelet is my favorite summer accessory. Not to mention they are incredibly easy to make. Miyuki Tila beads are little square and rectangular glass beads and literally come in every color you can possibly imagine, which makes playing around with all kinds of amazing color combinations very addictive.",
+        project_image="/images/jewelry/tila-bracelet/tilabeadproject1.jpeg",
         category_id=4,
         supplies_text="- Miyuki Tila beads (an assortment of quarter, half and square Tila beads) - stretch cord (0.5mm for a softer stretch and 0.7mm for a tighter stretch) - scissors or cutters - clear nail polish",
         supplies_image="/images/jewelry/tila-bracelet/tilabeadproject1.jpeg"
@@ -34,6 +36,7 @@ def seed_projects():
         user_id=10,
         title="Beaded Daisy Chain Bracelet",
         description="Honestly, is there anything happier than daisies?! I was desperately needing a dose of happy over the weekend and whipped up a few colorful bracelets to soothe the soul. Making daisy chains from seed beads might look complicated but honestly, it's not difficult at all! It's a cinch as long as you have the right materials. I dare you to stop at one, it's that addicting. Just be sure to pay attention to the size of the beads and stringing cord before you purchase the supplies. Have fun!",
+        project_image="/images/jewelry/daisy-bracelet/daisychainproject1.jpeg",
         category_id=4,
         supplies_text="- 8/0 seed beads, assorted colors (I used frosted innocent pink, frosted cantelope, opaque sunshine, frosted shamrock, frosted turquoise, frosted periwinkle and frosted light beige) - no.8 Griffin silk thread with needle - 3mm brass bead (optional) - no.10 Griffin silk thread or 0.8-1mm Chinese knotting cord (for closure) - scissors - pliers (optional) - materials for other bracelets - freshwater pearls - African beads - Czech beads - 0.5mm elastic string - 11/0 gold seed beads",
         supplies_image="/images/jewelry/daisy-bracelet/daisychainproject1.jpeg"
@@ -43,6 +46,7 @@ def seed_projects():
         user_id=10,
         title="Mother of Pearl Necklace",
         description="Summer is just weeks away and although we don't quite know what the near future will look like just yet, I'm looking forward for easing up on just about every aspect of life. I recently stumbled upon these adorable mother of pearl charms and the idea of incorporating them into some necklaces tickled me pink. They're just perfect for summer!",
+        project_image="/images/jewelry/mother-of-pearl-necklace/motherofpearlproject1.jpeg",
         category_id=4,
         supplies_text="- mother of pearl letter charms - mother of pearl evil eye clover charm - mother of pearl hamsa charm - mother of pearl bows, bunnies and zodiac - ombré gemstone rondelle beads (pink opal, rainbow sapphire, tourmaline and blue sapphires) - freshwater pearls - gold spacer beads - 4mm gold filled crimp bead covers - 6mm closed jump rings - 7 x 12mm lobster clasps - metallic flex wire in gold - 0.5mm waxed cord (for single bead necklaces) - chain nose pliers - crimping tool - scissors - scotch tape",
         supplies_image="/images/jewelry/mother-of-pearl-necklace/motherofpearlproject1.jpeg"
@@ -52,6 +56,7 @@ def seed_projects():
         user_id=10,
         title="Beaded Shell Necklaces",
         description="Throughout the years, I've collected an endless stands of beads from all over the world - from my jewelry designing days to my present Etsy trolling days. After stumbling upon a few pretty gold shell charms a couple of weeks ago, I knew I had to put some of my favorite beads to good use. After all, is there anything that epitomizes summer more than a layered stack of beaded shell necklaces? I think not . . .",
+        project_image="/images/jewelry/shell-necklace/shellsproject1.jpg",
         category_id=4,
         supplies_text="- gold shell charms (sea star charms, sea shells, cowrie shells) - beads (bone heshi, African record disc heshi beads, Ghana glass beads) - beading cord - crimp beads - gold clasps - gold closed jump rings - bead crimper - embroidery needle - tassels (optional)",
         supplies_image="/images/jewelry/shell-necklace/shellsproject1.jpg"
