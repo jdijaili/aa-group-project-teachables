d93a87af4bd6002ab7207d849fdc1536e03b3aec
jdijaili <jennifer.dijailirx@gmail.com>
2022-02-10 10:51:36 -0800

    completed search route

--- app/api/search_routes.py
+++ app/api/search_routes.py
@@ -1,12 +1,14 @@
-from flask import Blueprint
-from app.models import db, Project
+from flask import Blueprint, jsonify
+from app.models import Project
 from app.forms.search_form import SearchForm
 
 search_routes = Blueprint("search", __name__)
 
-@search_routes.route("/<string:query>(\\w+)", methods=["POST"])
+@search_routes.route("/<string:query>(\\w+)")
 def search_projects():
     form = SearchForm()
 
     if form.validate_on_submit():
         search_query = form.data['search']
+        search_results = Project.query.filter(Project.title.ilike(f'%{search_query}%')).all()
+        return jsonify([project.to_JSON() for project in search_results])
