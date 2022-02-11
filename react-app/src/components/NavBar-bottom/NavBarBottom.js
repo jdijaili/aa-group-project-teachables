d80a15146eb6b887a12b0f19290a5d512f76f31e
Nathaniel Tseng <nathanieltseng@gmail.com>
2022-02-11 11:40:24 -0800

    restored searchbar to bottom navbar

--- react-app/src/components/NavBar-bottom/NavBarBottom.js
+++ react-app/src/components/NavBar-bottom/NavBarBottom.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SearchBar from '../SearchBar/SearchBar';
 import './NavBarBottom.css';
 
 const NavBarBottom = () => {
-    return (
-        <ul className='navbar-bottom'>
-            <div className='navbar-bottom-left'>
-                <li className='logo'><Link to='/'><img src='https://res.cloudinary.com/jenn/image/upload/v1644443029/teachables/teachable-logo_vaflfj.png' alt='teachable-logo' style={{ height: '50px', padding: '0 10px 0 0'}} /></Link></li>
-                <li><Link to='/' style={{color: 'black'}}>teachables</Link></li>
-            </div>
-            <div className='navbar-bottom-right'>
-                <li><Link to='/publish' style={{color: 'black'}}>PUBLISH</Link></li>
-                <li>SEARCH BAR</li>
-            </div>
-        </ul>
-    )
+	return (
+		<ul className='navbar-bottom'>
+			<div className='navbar-bottom-left'>
+				<li className='logo'><Link to='/'><img src='https://res.cloudinary.com/jenn/image/upload/v1644443029/teachables/teachable-logo_vaflfj.png' alt='teachable-logo' style={{ height: '50px', padding: '0 10px 0 0' }} /></Link></li>
+				<li><Link to='/' style={{ color: 'black' }}>teachables</Link></li>
+			</div>
+			<div className='navbar-bottom-right'>
+				<li><Link to='/publish' style={{ color: 'black' }}>PUBLISH</Link></li>
+				<SearchBar />
+			</div>
+		</ul>
+	)
 };
 
 export default NavBarBottom;
